baac87903c5549d9155ca9ee01a7a5d0e25e9bb7
taniiicom <mail@taniii.com>
2024-12-31 13:02:45 +0900

    add: app.ts > hello-world

---
+++ server/app.ts
@@ -0,0 +1,21 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({ message: "Hello, TypeScript Express!" });
+});
+
+const port = process.env.PORT || 8000;
+try {
+  app.listen(port, () => {
+    console.log(`Running at Port ${port}...`);
+  });
+} catch (e) {
+  if (e instanceof Error) {
+    console.error(e.message);
+  }
+}
