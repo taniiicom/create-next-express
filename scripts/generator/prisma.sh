1c647b0483704330b3e33a28d7d36bc61240d313
taniiicom <mail@taniii.com>
2025-01-01 18:48:24 +0900

    up: prisma.sh > 実行ディレクトリを調整

--- scripts/generator/prisma.sh
+++ scripts/generator/prisma.sh
@@ -2,7 +2,15 @@
 
 # Prisma クライアントのインストールと型生成
 npm install --save-dev prisma
+
+# server 内に移動
+cd packages/server
+
+# prisma client の生成
 npx prisma generate
 
+# ルートディレクトリに移動
+cd ../../
+
 # 型のコピーまたはシンボリックリンク作成
-cp -r packages/server/node_modules/.prisma/client packages/shared/prisma-types
+cp -r packages/server/node_modules/.prisma/client packages/shared/prisma/types
